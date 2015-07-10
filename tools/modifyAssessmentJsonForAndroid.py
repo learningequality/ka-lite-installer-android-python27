f53a7e19220dbce93d5aaca23651678a1e13b861
66eli77 <66eli77@gmail.com>
2015-07-10 14:59:01 -0700

    update the tools folder

--- tools/modifyAssessmentJsonForAndroid.py
+++ tools/modifyAssessmentJsonForAndroid.py
@@ -15,8 +15,8 @@ f = open('processed-assessmentitems.json', 'w')
 for i, val in enumerate(data):
 	if i > 0:
 		val = create_subfolder_name(val) + val
-	if len(data) > i + 1:
-		val = val + 'file:///android_asset/khan/'
+	# if len(data) > i + 1:
+	# 	val = val + 'file:///android_asset/khan/'
 	# print '@@@@@@    ', val
 	f.write(val)
 	
